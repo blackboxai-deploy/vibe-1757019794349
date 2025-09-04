2e3086bb81e48d515d8fed8aa1c0ae820569910c
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-05 02:33:24 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Game from "@/components/Game";
+
+export default function Home() {
+  return (
+    <main className="relative flex min-h-screen flex-col items-center justify-center">
+      <Game />
+    </main>
+  );
+}
